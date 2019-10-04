27b8612e5354ff1987c3afcf14f72709256815ac
Eduardo Meneses <eduardo.demeneses@gmail.com>
2019-10-04 14:20:16 +0200

    first commit

---
+++ src/main/java/com/duda/quantasvezeselapartiu/QuantasVezesElaPartiuApplication.java
@@ -0,0 +1,13 @@
+package com.duda.quantasvezeselapartiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantasVezesElaPartiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantasVezesElaPartiuApplication.class, args);
+	}
+
+}
