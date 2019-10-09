49c3f94c03baf2d66faece8f663264c1809c565d
Eduardo Meneses <eduardo.demeneses@gmail.com>
2019-10-09 15:44:54 +0200

    adding nested route from path

--- src/main/java/com/duda/quantasvezeselapartiu/configuration/endpoint/ElaPartiuEndpointConfiguration.java
+++ src/main/java/com/duda/quantasvezeselapartiu/configuration/endpoint/ElaPartiuEndpointConfiguration.java
@@ -4,9 +4,11 @@ import com.duda.quantasvezeselapartiu.handler.ElaPartiuHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.path;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -14,9 +16,9 @@ public class ElaPartiuEndpointConfiguration {
 
     @Bean
     public RouterFunction<ServerResponse> routes(ElaPartiuHandler handler){
-        return  route(
-                    GET("/v1/ela-partiu"),
-                    handler::quantasVezes
+        return RouterFunctions.nest(
+                path("/v1"),
+                route(GET("/ela-partiu"), handler::quantasVezes)
         );
     }
 
